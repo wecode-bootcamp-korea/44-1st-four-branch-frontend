b9a99a20f5dabe39499e883dd6395ff401994d22
KIMYOUNGWOON <youngwn2130@gmail.com>
2023-04-03 18:10:47 +0900

    ADD: 초기세팅픽스

--- src/pages/Main/ BandBanner/BandBanner.js
+++ src/pages/Main/ BandBanner/BandBanner.js
@@ -6,7 +6,7 @@ function BandBanner() {
     <div className="bandBanner">
       <div>
         전 구매 무료 배송과 선물 포장 혜택을 즐겨보세요.
-        &nbsp;&nbsp;&nbsp;&nbsp;+
+        <span>+</span>
       </div>
     </div>
   );
