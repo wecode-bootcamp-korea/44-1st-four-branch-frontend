6cfeb5031ae81cea8b0f5e93d709fa2c55f7f2d2
박지연 <parkjiyeoun@bagjiyeon-ui-MacBookPro.local>
2023-04-04 12:01:14 +0900

    ADD: 로그인 레이아웃 진행중

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import './Login.scss';
+import { FiX } from 'react-icons/fi';
 
 function Login() {
-  return <div></div>;
+  return (
+    <div className="login">
+      <div className="container">
+        <div className="icon-handle-page">
+          <FiX className="icon-20 close-button" />
+        </div>
+        <h3 className="title">로그인</h3>
+        <form>
+          <input className="email input-w500" placeholder="이메일 주소" />
+          <div className="password">
+            <input
+              className="input-w500"
+              placeholder="비밀번호"
+              type="password"
+            />
+            <button className="password-type-button">보기</button>
+          </div>
+        </form>
+        <button className="primary-button">로그인</button>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import BandBanner from './ BandBanner/BandBanner';
-
+import Login from '../../Components/Login';
 function Main() {
   return (
     <div>
+      <Login />
       <BandBanner />
     </div>
   );
