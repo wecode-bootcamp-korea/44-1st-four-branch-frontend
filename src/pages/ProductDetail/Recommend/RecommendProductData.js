0ab2dc8441ac9819c2f888219e05d000a2c77de4
박지연 <parkjiyeoun@bagjiyeon-ui-MacBookPro.local>
2023-04-05 10:54:06 +0900

    ADD:제품상세페이지레이아웃  리뷰 수정

--- src/pages/ProductDetail/Recommend/RecommendProductData.js
+++ src/pages/ProductDetail/Recommend/RecommendProductData.js
@@ -19,24 +19,32 @@ export const RECOMMEND_PRODUCT_LIST = [
     id: 3,
     name: '휠 오 드 퍼퓸',
     description: '이끼, 안개, 히바 나무',
+    image:
+      'https://www.aesop.com/u1nb1km7t5q7/4HSK3XP7EifCH3zX1dpEz8/a702430fe70a11d5941c57bb3e4b0dfd/Aesop-Reverence-Duet-with-Product-Large-1584x962px.png',
     link: 'https://www.aesop.com/kr/p/body-hand/hand/resurrection-aromatique-hand-wash/',
   },
   {
     id: 4,
     name: '카모마일 컨센트레이트 안티 블레미쉬 마스크',
     description: '문제성 피부를 위한 클레이 기반의 클렌징 제품',
+    image:
+      'https://www.aesop.com/u1nb1km7t5q7/0aGUOBL4ZtXEOobUwZU0J/5c2a6110f5543a72adc711e15e014bbf/Aesop-Skin-Chamomile-Concentrate-Anti-Blemish-Masque-60mL-large.png',
     link: 'https://www.aesop.com/kr/p/body-hand/hand/resurrection-aromatique-hand-wash/',
   },
   {
     id: 5,
     name: '파슬리 씨드 안티 옥시던트 인텐스 세럼',
     description: '지속적인 수분을 공급하는 항산화 세럼',
+    image:
+      'https://www.aesop.com/u1nb1km7t5q7/18oCIrIJkaPhgw23dVa6xz/29244f678a3fc2b0308a0edac55b2171/Aesop_Skin_Parsley_Seed_Anti-Oxidant_Intense_Serum_60mL_Web_Large_684x668px.png',
     link: 'https://www.aesop.com/kr/p/body-hand/hand/resurrection-aromatique-hand-wash/',
   },
   {
     id: 6,
     name: '테싯 오 드 퍼퓸',
     description: '활기찬 기운을 불어넣는 현대적인 향수',
+    image:
+      'https://www.aesop.com/u1nb1km7t5q7/2ct0ouUr0ao2RJ6ZJS4uJn/951049b3301a0c82f4f1260e6951f8fb/Aesop_Fragrance_Hwyl_Eau_de_Parfum_50mL_Web_Large_1000x977px.png',
     link: 'https://www.aesop.com/kr/p/body-hand/hand/resurrection-aromatique-hand-wash/',
   },
 ];
