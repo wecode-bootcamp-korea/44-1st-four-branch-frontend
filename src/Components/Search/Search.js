2f766041f14f4cf73f01dc42d0acf2052ce5015e
박지연 <parkjiyeoun@bagjiyeon-ui-MacBookPro.local>
2023-04-06 18:31:07 +0900

    ADD:검색 레이아웃, 검색기능 구현중

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import './Search.scss';
+import SearchList from './SearchList/SearchList';
 import Logo from '../../assets/aesop-logo.png';
 import ProductImage from '../../assets/product(00).png';
 import { FiArrowRight } from 'react-icons/fi';
+import './Search.scss';
 
 function Search() {
+  const [searchList, setSearchList] = useState([]);
+  const [searchKeyword, setSearchKeyword] = useState('');
+
+  const itemSearch = e => {
+    setSearchKeyword(e.target.value);
+  };
+
+  const filteredItem = searchList.filter(item =>
+    item.name.toLowerCase().includes(searchKeyword.toLowerCase())
+  );
+
+  useEffect(() => {
+    fetch('/data/searchItemList.json')
+      .then(response => response.json())
+      .then(result => setSearchList(result));
+  }, []);
+
   return (
     <div className="search">
       <section className="searchWrap">
         <img src={Logo} alt="logo" />
         <form className="searchBox">
-          <input className="searchInput" />
+          <input
+            className="searchInput"
+            value={searchKeyword}
+            onChange={itemSearch}
+          />
           <FiArrowRight className="icon20 enterButton" />
         </form>
         <div className="hotKeyword">
@@ -21,7 +43,11 @@ function Search() {
         </div>
       </section>
       <section className="resultWrap">
-        <div className="searchList">
+        {searchList.map(item => (
+          <SearchList searchList={filteredItem} item={item} key={item.id} />
+        ))}
+
+        {/* <div className="searchList">
           <ul>
             <li>
               <Link to="/">구매하기</Link>
@@ -42,7 +68,7 @@ function Search() {
               <Link to="/">레버런스 아로마틱 핸드 밤</Link>
             </li>
           </ul>
-        </div>
+        </div> */}
       </section>
       <section className="productImage">
         <img src={ProductImage} alt="레저렉션 아로마틱 핸드 밤" />
--- src/Components/Search/SearchList/SearchList.js
+++ src/Components/Search/SearchList/SearchList.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import './SearchList.scss';
 
-function SearchList() {
-  return;
+function SearchList({ item }) {
+  const { name, imageUrl } = item;
+
+  return (
+    <div className="searchList">
+      <h2>관련 검색어</h2>
+      <h2>{name}</h2>
+      <img alt={name} src={imageUrl} />
+    </div>
+  );
 }
+
+export default SearchList;
