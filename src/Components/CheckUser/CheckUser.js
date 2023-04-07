e62cfbad94c9d28034cc1e08af23819f517d8890
KIMYOUNGWOON <youngwn2130@gmail.com>
2023-04-07 19:38:26 +0900

    ADD: 로그인 회원가입 레이아웃

---
+++ src/Components/CheckUser/CheckUser.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useState } from 'react';
+import UserModal from '../UserModal/UserModal';
+
+function CheckUser({ userInfoClose }) {
+  const [modalChange, setModalChange] = useState(LOGIN_DATA);
+
+  function modalChangehandle() {
+    setModalChange(SIGNUP_DATA);
+  }
+
+  function backhandle() {
+    setModalChange(LOGIN_DATA);
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+
+  return (
+    <UserModal
+      data={modalChange}
+      userInfoClose={userInfoClose}
+      modalChangehandle={modalChangehandle}
+      backhandle={backhandle}
+    />
+  );
+}
+
+export default CheckUser;
+
+const LOGIN_DATA = { title: '로그인', button: '로그인' };
+const SIGNUP_DATA = {
+  title: '회원가입',
+  button: '회원가입',
+};
