953aa1c0c9e0412615cdda3bb74075c1c41762fc
박지연 <parkjiyeoun@bagjiyeon-ui-MacBookPro.local>
2023-04-09 21:19:14 +0900

    ADD: 검색 기능 구현: 엔터키오류 해결

--- src/Components/Search/HoverItemView/HoverItemView.js
+++ src/Components/Search/HoverItemView/HoverItemView.js
@@ -4,10 +4,14 @@ import './HoverItemView.scss';
 function HoverItemView({ item }) {
   const { name, imageUrl } = item;
 
+  function moveItemPage() {}
+
   return (
     <div className="hoverItemView">
       <img className="hoverItemImage" alt={name} src={imageUrl} />
-      <button className="primarySolidButton">더 알아보기</button>
+      <button className="primarySolidButton" onClick={moveItemPage}>
+        더 알아보기
+      </button>
     </div>
   );
 }
--- src/Components/Search/SearchInput/SearchInput.js
+++ src/Components/Search/SearchInput/SearchInput.js
@@ -2,11 +2,22 @@ import React from 'react';
 import './SearchInput.scss';
 
 function SearchInput({ Logo, searchKeyword, itemSearch, FiArrowRight }) {
+  function preventReset(e) {
+    if (e.keyCode === 13) {
+      e.preventDefault();
+    }
+  }
+
   return (
     <div className="searchInput">
       <img src={Logo} alt="logo" />
       <form className="searchBox">
-        <input className="input" value={searchKeyword} onChange={itemSearch} />
+        <input
+          className="input"
+          value={searchKeyword}
+          onChange={itemSearch}
+          onKeyDown={preventReset}
+        />
         <FiArrowRight className="icon20 enterButton" />
       </form>
     </div>
