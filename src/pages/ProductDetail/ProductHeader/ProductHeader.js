69c065bd8337baf359317394ea8d254913ad42e9
박지연 <parkjiyeoun@bagjiyeon-ui-MacBookPro.local>
2023-04-10 10:06:03 +0900

    ADD: 제픔상세페이지 구현

--- src/pages/ProductDetail/ProductHeader/ProductHeader.js
+++ src/pages/ProductDetail/ProductHeader/ProductHeader.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
 import { FiPlusCircle } from 'react-icons/fi';
 import Logo from '../../../assets/ProductDetail/aesop-logo.png';
 import ProductWishList from './ProductWishList/ProductWishList';
@@ -10,11 +9,7 @@ import './ProductHeader.scss';
 
 function ProductHeader({ isOpenModal, modalView, slide, isCloseModal }) {
   const [productDetailList, setProductDetailList] = useState([]);
-  const [product, setProduct] = useState({});
   const [wishList, setWishList] = useState(false);
-
-  const params = useParams();
-  const productId = params.id;
 
   function handleWIshList() {
     setWishList(wishList => !wishList);
@@ -29,36 +24,15 @@ function ProductHeader({ isOpenModal, modalView, slide, isCloseModal }) {
     //   .then(response => response.json())
     //   .then(result => setProductDetailList(result));
 
-    // fetch('http://10.58.52.90:3000/products?pid=1', {
-    //   // method: 'GET',
-    //   headers: {
-    //     'Content-Type': 'application/json;charset=utf-8',
-    //   },
-    // })
-    //   .then(response => response.json())
-    //   .then(result => setProductDetailList(result));
-
-    fetch('http://10.58.52.90:3000/products/pid=${productId}', {
+    fetch('http://10.58.52.90:3000/products?pid=1', {
       // method: 'GET',
       headers: {
         'Content-Type': 'application/json;charset=utf-8',
       },
     })
       .then(response => response.json())
-      .then(result => setProduct(result));
-  }, [productId]);
-
-  const {
-    ingredients,
-    id,
-    imageUrl,
-    name,
-    mainCategoryName,
-    subCategoryName,
-    description,
-    size,
-    price,
-  } = product;
+      .then(result => setProductDetailList(result));
+  }, []);
 
   return (
     <div>
@@ -69,68 +43,67 @@ function ProductHeader({ isOpenModal, modalView, slide, isCloseModal }) {
           isCloseModal={isCloseModal}
         />
       )}
-      {product.id &&
-        product.map(
-          ({
-            ingredients,
-            id,
-            imageUrl,
-            name,
-            mainCategoryName,
-            subCategoryName,
-            description,
-            size,
-            price,
-          }) => {
-            return (
-              <div className={`productHeader ${slide}`} key={id}>
-                <section className="logoContainer">
-                  <img className="logo" src={Logo} alt="logo" />
-                </section>
-                <section className="productImageContainer">
-                  <img className="productImage" src={imageUrl} alt={name} />
-                </section>
-                <section className="productDetailContainer">
-                  <div className="productTitleContainer">
-                    <div className="productCategoryLink">
-                      {mainCategoryName}
-                      <div className="divideDot" />
-                      {subCategoryName}
-                    </div>
-                    <h2 className="productName">{name}</h2>
-                    <p className="productDescription">{description}</p>
+      {productDetailList.map(
+        ({
+          ingredients,
+          id,
+          imageUrl,
+          name,
+          mainCategoryName,
+          subCategoryName,
+          description,
+          size,
+          price,
+        }) => {
+          return (
+            <div className={`productHeader ${slide}`} key={id}>
+              <section className="logoContainer">
+                <img className="logo" src={Logo} alt="logo" />
+              </section>
+              <section className="productImageContainer">
+                <img className="productImage" src={imageUrl} alt={name} />
+              </section>
+              <section className="productDetailContainer">
+                <div className="productTitleContainer">
+                  <div className="productCategoryLink">
+                    {mainCategoryName}
+                    <div className="divideDot" />
+                    {subCategoryName}
                   </div>
-                  <ul className="productDetail">
-                    <li className="productIngredient">
-                      <h6 className="ingredientTitle">주요 성분</h6>
-                      <p className="ingredientText">
-                        {makeIngredientList(ingredients)[0]},
-                        {makeIngredientList(ingredients)[1]},
-                        {makeIngredientList(ingredients)[2]}
-                      </p>
-                      <FiPlusCircle
-                        className="detailViewButton"
-                        onClick={isOpenModal}
-                      />
-                    </li>
-                    <li className="productSize">
-                      <h6 className="sizeTitle">사이즈</h6>
-                      <p className="sizeText">{size}</p>
-                    </li>
-                  </ul>
-                  <button className="primarySolidButton">{`카트에 추가하기 - ₩${Math.floor(
-                    price
-                  ).toLocaleString()}`}</button>
-                  <ProductWishList
-                    wishList={wishList}
-                    handleWishList={handleWIshList}
-                  />
-                  <ProductRelated />
-                </section>
-              </div>
-            );
-          }
-        )}
+                  <h2 className="productName">{name}</h2>
+                  <p className="productDescription">{description}</p>
+                </div>
+                <ul className="productDetail">
+                  <li className="productIngredient">
+                    <h6 className="ingredientTitle">주요 성분</h6>
+                    <p className="ingredientText">
+                      {makeIngredientList(ingredients)[0]},
+                      {makeIngredientList(ingredients)[1]},
+                      {makeIngredientList(ingredients)[2]}
+                    </p>
+                    <FiPlusCircle
+                      className="detailViewButton"
+                      onClick={isOpenModal}
+                    />
+                  </li>
+                  <li className="productSize">
+                    <h6 className="sizeTitle">사이즈</h6>
+                    <p className="sizeText">{size}</p>
+                  </li>
+                </ul>
+                <button className="primarySolidButton">{`카트에 추가하기 - ₩${Math.floor(
+                  price
+                ).toLocaleString()}`}</button>
+                <ProductWishList
+                  wishList={wishList}
+                  handleWishList={handleWIshList}
+                />
+                <ProductRelated />
+              </section>
+            </div>
+          );
+        }
+      )}
     </div>
   );
 }
