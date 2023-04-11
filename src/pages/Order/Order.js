7ad1bb5dfc83d3a8c00dacbd2dae9cda4e3e04b7
KIMYOUNGWOON <youngwn2130@gmail.com>
2023-04-11 12:29:46 +0900

    ADD: 주문 결제 페이지 레이아웃

--- src/pages/Order/Order.js
+++ src/pages/Order/Order.js
@@ -2,11 +2,22 @@ import React from 'react';
 import OrderInfo from './OrderInfo/OrderInfo';
 import OrderItem from './OrderItem/OrderItem';
 import './Order.scss';
+import { useState } from 'react';
 
 function Order() {
+  const [orderModal, setOrderModal] = useState('배송');
+
+  function movePayment() {
+    setOrderModal('결제');
+  }
+
   return (
     <div className="order">
-      <OrderInfo />
+      <OrderInfo
+        orderModal={orderModal}
+        setOrderModal={setOrderModal}
+        movePayment={movePayment}
+      />
       <OrderItem />
     </div>
   );
--- src/pages/Order/OrderInfo/OrderInfo.js
+++ src/pages/Order/OrderInfo/OrderInfo.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './OrderInfo.scss';
 
-function OrderInfo() {
+function OrderInfo({ orderModal, setOrderModal, movePayment }) {
   return (
     <div className="orderInfo">
       <nav className="orderNav">
@@ -20,50 +20,78 @@ function OrderInfo() {
           <div className="name">김영운 님</div>
           <div className="emailId">justreet@naver.com</div>
         </div>
-        <div className="deliveryInfo">
-          <div className="sendTo">주문을 어디로 보내시겠습니까?</div>
-          <div className="nameInput">
-            <div>
-              <div>
-                <label>성</label>
-              </div>
+        {orderModal === '배송' && (
+          <div className="deliveryInfo">
+            <div className="sendTo">주문을 어디로 보내시겠습니까?</div>
+            <div className="enterDeliveryInfo">
               <input
                 className="firstName"
                 name="firstName"
                 type="text"
                 placeholder="성"
               />
-            </div>
-            <div>
-              <div>
-                <label>이름</label>
-              </div>
               <input
                 className="lastName"
                 name="lastName"
                 type="text"
                 placeholder="이름"
               />
+              <input
+                type="tel"
+                className="phoneNumber"
+                name="phoneNumber"
+                placeholder="전화번호 (010-0000-0000)"
+              />
+              <input
+                className="postCode"
+                name="postCode"
+                placeholder="우편번호"
+              />
+              <input className="address" name="address" placeholder="주소" />
+              <input
+                className="betterAddress"
+                name="betterAddress"
+                placeholder="상세주소(아파트 동/호수, 일반주택 등)"
+              />
+              <button
+                className="moveBtn"
+                onClick={() => {
+                  movePayment();
+                }}
+              >
+                배송 정보로 이동
+              </button>
             </div>
           </div>
-          <div>
-            <label htmlFor="countryCode">국가 코드</label>
-            <select id="countryCode">
-              <option value="KOR">KOR</option>
-              <option>나열되지 않은 국가</option>
-            </select>
-            <input
-              className="phoneNumber"
-              name="phoneNumber"
-              placeholder="전화번호 (010-0000-0000)"
-            />
-          </div>
-          <label>국가</label>
-          <select>
-            <option>대한민국</option>
-            <option>나열되지 않은 국가</option>
-          </select>
-        </div>
+        )}
+        {orderModal === '결제' && (
+          <>
+            <div className="shippingAddress">
+              <div className="deliveryTitle"> 배송 정보 </div>
+              <div className="userName"> 김영운 </div>
+              <div className="userAddress"> 서울 강남구 테헤란로 427, 1층 </div>
+              <div className="userPostCode"> 06159 </div>
+              <div className="free"> 무료 배송 ₩0</div>
+            </div>
+            <div className="termsOfPayment">
+              <div className="paymentTitle"> 어떻게 지불하시겠습니까? </div>
+              <div>
+                <input
+                  name="payment"
+                  id="point"
+                  type="radio"
+                  checked="checked"
+                />
+                <label htmlFor="point">POINT 결제</label>
+              </div>
+              <div>
+                <input name="payment" id="card" type="radio" />
+                <label htmlFor="cart">카드 결제</label>
+              </div>
+              <button className="pageMove">다음 페이지</button>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
