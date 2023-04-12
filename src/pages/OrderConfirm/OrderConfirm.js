fc23578e0b35faad4d6b37572ce26b4278606b9c
박지연 <parkjiyeoun@bagjiyeon-ui-MacBookPro.local>
2023-04-12 16:32:00 +0900

    ADD:주문내역서 코드 수정

--- src/pages/OrderConfirm/OrderConfirm.js
+++ src/pages/OrderConfirm/OrderConfirm.js
@@ -3,11 +3,19 @@ import Logo from '../../assets/fourbsopLogo.png';
 import './OrderConfirm.scss';
 
 function OrderConfirm() {
-  const [orderInfo, setOrderInfo] = useState([]);
-  let totalAmount = [];
+  const [orderInfo, setOrderInfo] = useState({});
 
-  function makeOrderInfo(value) {
-    return JSON.parse(value);
+  let userAddressObject = {};
+  const userAddressString = orderInfo?.userAddress;
+  if (userAddressString && userAddressString !== undefined) {
+    userAddressObject = JSON.parse(userAddressString);
+  }
+
+  let totalAmount = [];
+  let orderItemsArray = [];
+  const orderItemsString = orderInfo?.orderItems;
+  if (orderItemsString && orderItemsString !== undefined) {
+    [...orderItemsArray] = JSON.parse(orderItemsString);
   }
 
   // const token = localStorage.getItem('token');
@@ -23,83 +31,77 @@ function OrderConfirm() {
       },
     })
       .then(response => response.json())
-      .then(result => setOrderInfo(result));
+      .then(result => setOrderInfo(result[0]));
   }, []);
 
   return (
     <div className="orderConfirm">
-      {orderInfo.map((info, index) => {
-        return (
-          <div className="orderConfirmWrap" key={index}>
-            <section className="logoContainer">
-              <img className="logo" src={Logo} alt="로고" />
-            </section>
-            <section className="contents">
-              <h2 className="comment">{`${info.userFirstName} ${info.userLastName} 님 감사합니다.`}</h2>
-              <p>
-                {`감사합니다. ${info.userFirstName} ${info.userLastName} 님. 주문이 완료되었습니다. 추가 문의 사항이
+      <div className="orderConfirmWrap">
+        <section className="logoContainer">
+          <img className="logo" src={Logo} alt="로고" />
+        </section>
+        <section className="contents">
+          <h2 className="comment">{`${orderInfo.userFirstName} ${orderInfo.userLastName} 님 감사합니다.`}</h2>
+          <p>
+            {`감사합니다. ${orderInfo.userFirstName} ${orderInfo.userLastName} 님. 주문이 완료되었습니다. 추가 문의 사항이
                 있으시면 연락 주시기 바랍니다.`}
-              </p>
+          </p>
 
-              <form>
-                <div className="orderNum">
-                  <div>
-                    <span>주문 번호 </span>
-                    <span className="num">{info.orderNumber}</span>
+          <form>
+            <div className="orderNum">
+              <div>
+                <span>주문 번호 </span>
+                <span className="num">{orderInfo.orderNumber}</span>
+              </div>
+              <p>{orderInfo.orderDate}</p>
+            </div>
+            <div className="detailInfo">
+              <div>
+                <h6 className="title">배송 정보</h6>
+                <p>{`${orderInfo.userFirstName} ${orderInfo.userLastName}`}</p>
+                <p>{userAddressObject.country}</p>
+                <p>{userAddressObject.postcode}</p>
+                <p>{userAddressObject.detail}</p>
+              </div>
+              <div>
+                <h6 className="title">주문 상태</h6>
+                <p>{orderInfo.orderStatus}</p>
+              </div>
+              <div>
+                <h6 className="title">지불 방법</h6>
+                <p>포인트</p>
+              </div>
+            </div>
+            <ul className="bills">
+              {orderItemsArray.map((item, index) => (
+                <li key={index}>
+                  <p className="productName">{item.productName}</p>
+                  <span>{item.size}</span>
+                  <div className="productPrice">
+                    <span>{`₩ ${Math.floor(
+                      item.price
+                    ).toLocaleString()}`}</span>
+                    <span>{` x ${item.quantity}`}</span>
                   </div>
-                  <p>{info.orderDate}</p>
-                </div>
-                <div className="detailInfo">
-                  <div>
-                    <h6 className="title">배송 정보</h6>
-                    <p>{`${info.userFirstName} ${info.userLastName}`}</p>
-                    <p>{makeOrderInfo(info.userAddress).country}</p>
-                    <p>{makeOrderInfo(info.userAddress).postcode}</p>
-                    <p>{makeOrderInfo(info.userAddress).detail}</p>
-                  </div>
-                  <div>
-                    <h6 className="title">주문 상태</h6>
-                    <p>{info.orderStatus}</p>
-                  </div>
-                  <div>
-                    <h6 className="title">지불 방법</h6>
-                    <p>포인트</p>
-                  </div>
-                </div>
-                <ul className="bills">
-                  {makeOrderInfo(info.orderItems).map((item, index) => (
-                    <li key={index}>
-                      <p className="productName">{item.productName}</p>
-                      <span>{item.size}</span>
-                      <div className="productPrice">
-                        <span>{`₩ ${Math.floor(
-                          item.price
-                        ).toLocaleString()}`}</span>
-                        <span>{` x ${item.quantity}`}</span>
-                      </div>
-                    </li>
-                  ))}
-                  <li className="totalPrice">
-                    <span>합계</span>
-                    <span className="totalNum">
-                      {`₩ ${Math.floor(
-                        (totalAmount = makeOrderInfo(
-                          orderInfo[0].orderItems
-                        ).reduce(
-                          (accumulator, currentValue) =>
-                            accumulator +
-                            currentValue.price * currentValue.quantity,
-                          0
-                        ))
-                      ).toLocaleString()}`}
-                    </span>
-                  </li>
-                </ul>
-              </form>
-            </section>
-          </div>
-        );
-      })}
+                </li>
+              ))}
+              <li className="totalPrice">
+                <span>합계</span>
+                <span className="totalNum">
+                  {`₩ ${Math.floor(
+                    (totalAmount = orderItemsArray.reduce(
+                      (accumulator, currentValue) =>
+                        accumulator +
+                        currentValue.price * currentValue.quantity,
+                      0
+                    ))
+                  ).toLocaleString()}`}
+                </span>
+              </li>
+            </ul>
+          </form>
+        </section>
+      </div>
     </div>
   );
 }
