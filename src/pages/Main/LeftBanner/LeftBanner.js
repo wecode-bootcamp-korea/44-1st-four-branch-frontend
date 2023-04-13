e870462b86a1a50bfcebe552e46c38544ae48050
KIMYOUNGWOON <youngwn2130@gmail.com>
2023-04-13 14:49:47 +0900

    ADD 상품리스트 페이지 애니메이션

--- src/pages/Main/LeftBanner/LeftBanner.js
+++ src/pages/Main/LeftBanner/LeftBanner.js
@@ -19,7 +19,7 @@ function LeftBanner() {
       <div className="articleImg" />
       <div className="articleContent">
         <div>프래그런스</div>
-        <h2 className="mainTitle">진실된 아름다움, 4bsop 소개</h2>
+        <h2 className="mainTitle">4bsop 스토어 소개</h2>
         <p className="content">
           피부 본연의 가치를 최우선으로 생각하는 사람들에게 진실되고 젤제된
           아름다움을 선사하고자 합니다.
--- src/pages/Main/MainBanner/MainBanner.js
+++ src/pages/Main/MainBanner/MainBanner.js
@@ -14,7 +14,7 @@ function MainBanner() {
             하며, 영향력 있게 새로운 트렌드를 만들어갑니다.
           </p>
           <div className="squareBox">
-            <div className="text">센티드 홈 보기</div>
+            <div className="text">트렌드 보기</div>
             <div className="arrow">→</div>
           </div>
         </div>
